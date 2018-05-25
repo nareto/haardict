fixed variance in similarity measures

plotsc.py
@@ -13,14 +13,15 @@ p750 = [patches[i] for i in np.random.permutation(range(len(patches)))][:750]
 p50 = [patches[i] for i in np.random.permutation(range(len(patches)))][:50]                                                
 p150 = [patches[i] for i in np.random.permutation(range(len(patches)))][:150]                                                
 #dd = td.learn_dict(['img/flowers_pool-rescale.npy'])                                                                         
-scemd750 = td.spectral_clustering(p750,1e-4,'emd')
-scemd50 = td.spectral_clustering(p50,1e-4,'emd')
-schaar50 = td.spectral_clustering(p50,1e-4,'haarpsi')
-schaar750 = td.spectral_clustering(p750,1e-4,'haarpsi')
-scfro750 = td.spectral_clustering(p750,1e-4,'frobenius')
+#scemd750 = td.spectral_clustering(p750,1e-4,'emd')
+#scemd50 = td.spectral_clustering(p50,1e-4,'emd')
+#schaar50 = td.spectral_clustering(p50,1e-4,'haarpsi')
+#schaar750 = td.spectral_clustering(p750,1e-4,'haarpsi')
+#scfro750 = td.spectral_clustering(p750,1e-4,'frobenius')
 
 def exec(loadpickle=False):
-    for simm,beta in zip(['frobenius','haarpsi','emd'],[0.06,1,0.001]):
+    #for simm,beta in zip(['frobenius','haarpsi','emd'],[1e4,1e3,1e4]):
+    for simm,beta in zip(['frobenius','emd','haarpsi'],[1,1,1e-5]):
         sc = td.spectral_clustering(p750,1e-4,simm,simmeasure_beta=beta)
         codename = 'scp750clust-seed:'+str(seed)+'-'+simm+'.pickle'
         if loadpickle:
@@ -59,7 +60,6 @@ def plotegv(clust,savep=None):
         plt.savefig(savep)
     else:
         plt.show()
-
 
 
 

twoDdict.py
@@ -139,11 +139,16 @@ def simmeasure_haarpsi(reshape=False):
     return(dh)
 
 #beta=0.06
-def simmeasure_frobenius(beta=0.06,datavar=1):
+def simmeasure_frobenius(beta=0.06,samples=None):
+    if samples is None:
+        datavar = 1
+    else:
+        avg = sum(samples)/len(samples)
+        datavar = sum([np.linalg.norm(p-avg,ord='fro')**2 for p in samples])/len(samples)
     ret = lambda patch1,patch2: np.exp(-beta*(np.linalg.norm(patch1 - patch2,ord='fro')**2)/datavar)
     return(ret)
 
-def simmeasure_emd(patch_size,beta=0.06,datavar=1):
+def simmeasure_emd(patch_size,beta=0.06,samples=None):
     prows,pcols = patch_size
     histlength = prows*pcols
     metric_matrix = np.zeros((histlength,histlength))
@@ -154,7 +159,12 @@ def simmeasure_emd(patch_size,beta=0.06,datavar=1):
         p2 = np.array([row2,col2])
         metric_matrix[i,j] = np.linalg.norm(p1-p2)
     metric_matrix += metric_matrix.transpose()
-    ret = lambda patch1,patch2: np.exp(-beta*(pyemd.emd(patch1.flatten(),patch2.flatten(),metric_matrix)**2))
+    if samples is None:
+        datavar = 1
+    else:
+        avg = sum(samples)/len(samples)
+        datavar = sum([pyemd.emd(p.astype('float64').flatten(),avg.astype('float64').flatten(),metric_matrix)**2 for p in samples])/len(samples)
+    ret = lambda patch1,patch2: np.exp(-beta*(pyemd.emd(patch1.astype('float64').flatten(),patch2.astype('float64').flatten(),metric_matrix)**2)/datavar)
     return(ret)
 
 def centroid(values):
@@ -629,9 +639,9 @@ class Cluster(Saveable):
         if self.similarity_measure == 'haarpsi':
             sim = simmeasure_haarpsi()
         elif self.similarity_measure == 'frobenius':
-            sim = simmeasure_frobenius(beta=self.simmeasure_beta,datavar=np.var(self.samples))
+            sim = simmeasure_frobenius(beta=self.simmeasure_beta,samples=self.samples)
         elif self.similarity_measure == 'emd':
-            sim  = simmeasure_emd(self.patch_size,beta=self.simmeasure_beta,datavar=np.var(self.samples))
+            sim  = simmeasure_emd(self.patch_size,beta=self.simmeasure_beta,samples=self.samples)
 
         self.affinity_matrix = affinity_matrix(self.samples,sim,self.affinity_matrix_threshold)
         #print(len(self.affinity_matrix.data)/(self.affinity_matrix.shape[0]*self.affinity_matrix.shape[1]))